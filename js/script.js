d80f5d5c2a55b30cf9f096c70fe39bb04b0b8c17
s1f102100119 <s1f102100119@iniad.org>
2023-09-25 01:52:39 +0900

    現在ポートフォリオ保存

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
+  //slickオプション
+  $(".slick").slick({
+    dots: true,
+    arrows: true,
+    swipe: true, 
+  });
+
+  //lightboxオプション
+  lightbox.option({
+    alwaysShowNavOnTouchDevices:true,	
+    wrapAround: true,
+    showImageNumberLabel:false
+  })
+
+});
+
